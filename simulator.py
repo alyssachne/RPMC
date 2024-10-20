01562a0e076d89691a6d48e0d68887684a3df7fc
Alyssa <yuqi.chen@mail.utoronto.ca>
2024-10-19 21:12:09 -0400

    put transaction count into csv

--- simulator.py
+++ simulator.py
@@ -60,10 +60,14 @@ class Simulator:
     
         # Convert list of dictionaries to a DataFrame
         df = pd.DataFrame(data)
+
+        # put the total transaction count in the last row
+        df.loc[len(df.index)] = ['Total Transaction Count', self.trader.get_transaction_count(), '', '']
         
         # Write DataFrame to CSV file
         timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
         df.to_csv(f'data/results/trader_results_{timestamp}_cash_level{str(self.cash_level)}.csv', index=False)
+
 if __name__ == "__main__":
     simulator = Simulator('data/daily_tickers.json', 'data/price_df_all_tickers.csv', 0.1, 10000)
     simulator = Simulator('data/daily_tickers.json', 'data/price_df_all_tickers.csv', 0.05, 10000)
